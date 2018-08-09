5b165ebfbe20599bdc4885d0f093a4d7b33e54b7
jim <j.matthews-14@student.lboro.ac.uk>
2018-08-09 12:27:28 +0100

    no default values for __init__ but can still create instance of class without params declared

--- packetstatistics.py
+++ packetstatistics.py
@@ -13,12 +13,18 @@ class PacketStatistics:
     def __init__(self, **kwargs):
         # These are object variables, they aren't initialised until after the object is defined.
         # Use kwargs so they don't have to be initialised in the correct order.
-        # Set to default to 0 if they aren't set in the class object?
-        self._sw = kwargs['sw'] if 'sw' in kwargs else 0
-        self._median = kwargs['median'] if 'median' in kwargs else 0
-        self._lower_quart = kwargs['lower_quart'] if 'lower_quart' in kwargs else 0
-        self._upper_quart = kwargs['upper_quart'] if 'upper_quart' in kwargs else 0
-        self._inter_quart = kwargs['inter_quart_range'] if 'inter_quart_range' in kwargs else 0
+        # Set to default to 0 if they aren't set in the class instance?
+        # self._sw = kwargs['sw'] if 'sw' in kwargs else 0
+        # self._median = kwargs['median'] if 'median' in kwargs else 0
+        # self._lower_quart = kwargs['lower_quart'] if 'lower_quart' in kwargs else 0
+        # self._upper_quart = kwargs['upper_quart'] if 'upper_quart' in kwargs else 0
+        # self._inter_quart = kwargs['inter_quart_range'] if 'inter_quart_range' in kwargs else 0
+
+        if 'sw' in kwargs: self._sw = kwargs['sw']
+        if 'median' in kwargs: self._median = kwargs['median']
+        if 'lower_quart' in kwargs: self._lower_quart = kwargs['lower_quart']
+        if 'upper_quart' in kwargs: self._upper_quart = kwargs['upper_quart']
+        if 'inter_quart_range' in kwargs: self._inter_quart_range = kwargs['inter_quart_range']
 
     # These functions are called methods in Python classes
     @staticmethod
@@ -45,4 +51,4 @@ class PacketStatistics:
         sorted_data = np.sort(data)
         self._lower_quart = sorted_data[int((self._sw + 1) / 4)]
         self._upper_quart = sorted_data[int(3 * ((self._sw + 1) / 4))]
-        self._inter_quart = int(self._upper_quart - self._lower_quart)
+        self._inter_quart_range = int(self._upper_quart - self._lower_quart)
