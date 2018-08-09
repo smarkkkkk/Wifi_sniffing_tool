49568d8ec62a3f2d4bd047760a42739b431b6f16
jim <j.matthews-14@student.lboro.ac.uk>
2018-08-09 14:36:32 +0100

    staticmethod removed from functions and data now passed in to every class instance

--- packetstatistics.py
+++ packetstatistics.py
@@ -10,15 +10,14 @@ class PacketStatistics:
     # when initiating the class
 
     # def __init__(self, sw, median, lower_quart, upper_quart, inter_quart_range):
-    def __init__(self, **kwargs):
+    def __init__(self, data, **kwargs):
         # These are object variables, they aren't initialised until after the object is defined.
         # Use kwargs so they don't have to be initialised in the correct order.
         # Set to default to 0 if they aren't set in the class instance?
-        # self._sw = kwargs['sw'] if 'sw' in kwargs else 0
-        # self._median = kwargs['median'] if 'median' in kwargs else 0
-        # self._lower_quart = kwargs['lower_quart'] if 'lower_quart' in kwargs else 0
-        # self._upper_quart = kwargs['upper_quart'] if 'upper_quart' in kwargs else 0
-        # self._inter_quart = kwargs['inter_quart_range'] if 'inter_quart_range' in kwargs else 0
+        self._data = data
+        if 'mean' in kwargs: self._mean = kwargs['mean']
+        if 'dist_mean' in kwargs: self._dist_mean = kwargs['dist_mean']
+        if 'dist_maxval' in kwargs: self._dist_maxval = kwargs['dist_maxval']
 
         if 'sw' in kwargs: self._sw = kwargs['sw']
         if 'median' in kwargs: self._median = kwargs['median']
@@ -27,20 +26,22 @@ class PacketStatistics:
         if 'inter_quart_range' in kwargs: self._inter_quart_range = kwargs['inter_quart_range']
 
     # These functions are called methods in Python classes
-    @staticmethod
-    def mean(data):
-        return statistics.mean(data)
+    #@staticmethod
+    def mean(self):
+        self._mean = statistics.mean(self._data)
 
-    @staticmethod
-    def frequency(data):
-        return statistics.mode(data)
+    #@staticmethod
+    def frequency(self):
+        return statistics.mode(self._data)
 
-    def median(self, data):
-        self._median = statistics.median(data)
+    def median(self):
+        self._median = statistics.median(self._data)
 
-    @staticmethod
-    def distance(ave, new_val):
-        return abs(ave - new_val)
+    #@staticmethod
+    def distance(self, new_val):
+        self._dist_mean = abs(self._mean - new_val)
+        self._dist_maxval = abs(max(self._data) - new_val)
+        #return abs(ave - new_val)
 
     def box_plot(self, data):
         # produces the quartiles and median, stores them in self.variable_name
