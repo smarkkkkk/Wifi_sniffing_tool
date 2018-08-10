3b81b2a19bc8f881637adf6a4dd9d3be9df6b5f3
jim <j.matthews-14@student.lboro.ac.uk>
2018-08-10 17:03:36 +0100

    dynamic instances setup to access to class functions for each metric

--- feature_analysis.py
+++ feature_analysis.py
@@ -18,27 +18,27 @@ def metric_analysis(dbm_antsignal, datarate, duration, seq, ttl, sw_size):
     array_dict = {'dbm_antsignal': dbm_antsignal, 'datarate': datarate,
                   'duration': duration, 'seq': seq, 'ttl': ttl}
 
-    sw_dict = {'sw_dbm_antsignal': sw_dbm_antsignal, 'sw_datarate': sw_datarate,
-               'sw_duration': sw_duration, 'sw_seq': sw_seq, 'sw_ttl': sw_ttl}
-    ave_dict = {}
+    sw_dict = {'RSSI': sw_dbm_antsignal, 'Rate': sw_datarate,
+               'NAV': sw_duration, 'Seq': sw_seq, 'TTL': sw_ttl}
 
-    stats_1 = PacketStatistics
-    # loop over each array and calculate the mean/frequency for each array storing it in new dictionary
-    for k, v in sw_dict.items():
-        if k == 'sw_ttl':
-            ave_dict[k] = stats_1.frequency(v)
-            print(ave_dict[k])
-        else:
-            ave_dict[k] = stats_1.mean(v)
-            print(ave_dict[k])
-
-    stats_2 = PacketStatistics
+    # ave_dict = {}
+    #
+    # stats_1 = PacketStatistics
+    # # loop over each array and calculate the mean/frequency for each array storing it in new dictionary
+    # for k, v in sw_dict.items():
+    #     if k == 'sw_ttl':
+    #         ave_dict[k] = stats_1.frequency(v)
+    #         print(ave_dict[k])
+    #     else:
+    #         ave_dict[k] = stats_1.mean(v)
+    #         print(ave_dict[k])
+    #
+    # stats_2 = PacketStatistics
 
     # Loop through the arrays, extract the next packets one at a time
     # compare features with average values and then add or disregard packet
     # and then update averages
-    print('lenght of dbm_antsignal is {}'.format(len(dbm_antsignal)))
-    start, stop, step = sw_size + 1, len(dbm_antsignal), 1
+
 
     # counter = 0
 
@@ -56,18 +56,42 @@ def metric_analysis(dbm_antsignal, datarate, duration, seq, ttl, sw_size):
     # seq_bpa = AutoBPA(data=sw_seq, sw=sw_size)
     # ttl_bpa = AutoBPA(data=sw_ttl, sw=sw_size)
 
-    metric_2 = SelectMetrics(metric_val=sw_size)
-    features_to_analyse = metric_2.metric_combination()
+    metric = SelectMetrics(metric_val=sw_size)
+    features_to_analyse = metric.metric_combination()
 
     instance_dict = {}
+    # create an instance of each feature that is going to be analysed
+    # and store the instances in a dictionary 'instance_dict'
     for feature in features_to_analyse:
-        for features, arrays in sw_dict.items():
-            if feature == features:
-                print('Feature is : {}'.format(feature))
-                print(arrays)
+        for key, arrays in sw_dict.items():
+            if feature == key:
                 name = feature + '_bpa'
-                x = globals()[name] = AutoBPA(data=arrays, sw=25)  # sw must be set to sw_size in the function
-                # this value is just for testing
-                instance_dict[features] = x
+                print('Creating instance of AutoBPA class called {}.'
+                      'The Feature is : {}'.format(name, feature))
+                print(arrays)
+                # creates an instance of AutoBPA class
+                x = globals()[name] = AutoBPA(data=arrays, sw=sw_size)
+                instance_dict[key] = x
+
+    print(instance_dict)
+
+    print('lenght of dbm_antsignal is {}'.format(len(dbm_antsignal)))
+    start, stop, step = sw_size + 1, len(dbm_antsignal), 1
+
+    for incr in range(start, stop, step):
+        for k_1, arrays in array_dict.items():
+            for k_2, inst in instance_dict.items():
+
+                instance_dict[k_2].mean()
+                instance_dict[k_2].distance(arrays[incr])
+                instance_dict[k_2].box_plot()
+
+                n = instance_dict[k_2].normal(arrays[incr])
+                a = instance_dict[k_2].attack()
+                u = instance_dict[k_2].uncertainty()
+                phi = instance_dict[k_2].adjustment_factor()
+
+                
+
 
 
