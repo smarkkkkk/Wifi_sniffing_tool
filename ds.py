898abbce0f5736ed24ed525b63612b3e26d08ba8
jim <j.matthews-14@student.lboro.ac.uk>
2018-08-15 17:02:51 +0100

    DS class returns N,A,U dict to allow difference between N,A to be calculated outside

--- ds.py
+++ ds.py
@@ -3,12 +3,8 @@ from pyds.pyds import MassFunction
 
 class DempsterShafer(MassFunction):
 
-    def __init__(self, mass_dict, normal, attack, uncert, conflict):
+    def __init__(self, mass_dict):
         super().__init__(mass_dict)
-        self._n = normal
-        self._a = attack
-        self._u = uncert
-        self._conflict = conflict
 
     @staticmethod
     def process_ds(frozen_mass_func):
@@ -17,22 +13,9 @@ class DempsterShafer(MassFunction):
         ds_dict = {}
 
         conflict = 1 / (1 - list_mass_func[1][1])
-
         ds_dict['n'] = conflict * (list_mass_func[3][1] + list_mass_func[4][1])
-
         ds_dict['a'] = conflict * (list_mass_func[0][1] + list_mass_func[2][1])
-
         ds_dict['u'] = conflict * list_mass_func[5][1]
 
-        return max(ds_dict.keys(), key=(lambda key: ds_dict[key]))
-    
-
-
-
-
-
-
-
-
-
-
+        # max(ds_dict.keys(), key=(lambda key: ds_dict[key]))
+        return ds_dict
