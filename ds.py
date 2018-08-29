b3e611e5fb0ad825752f5769ccf0fb5facf7dd41
jim <j.matthews-14@student.lboro.ac.uk>
2018-08-29 17:27:14 +0100

    returned frozenset is now indexed like a dict to obtain N, A, U values

--- ds.py
+++ ds.py
@@ -5,14 +5,14 @@ class DempsterShafer:
 
     @staticmethod
     def process_ds(frozen_mass_func):
-        list_mass_func = list(frozen_mass_func.items())
+        # list_mass_func = list(frozen_mass_func.items())
 
         ds_dict = {}
 
-        conflict = 1 / (1 - list_mass_func[1][1])
-        ds_dict['n'] = conflict * (list_mass_func[3][1] + list_mass_func[4][1])
-        ds_dict['a'] = conflict * (list_mass_func[0][1] + list_mass_func[2][1])
-        ds_dict['u'] = conflict * list_mass_func[5][1]
+        conflict = 1 / (1 - frozen_mass_func['a', 'n'])
+        ds_dict['n'] = conflict * (frozen_mass_func['n'] + frozen_mass_func['n', 'u'])
+        ds_dict['a'] = conflict * (frozen_mass_func['a'] + frozen_mass_func['a', 'u'])
+        ds_dict['u'] = conflict * frozen_mass_func['u']
 
         # max(ds_dict.keys(), key=(lambda key: ds_dict[key]))
         return ds_dict
