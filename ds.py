85c197f8d45d27611d8718134d30fe1b27ab70fc
jim <j.matthews-14@student.lboro.ac.uk>
2018-08-30 11:40:21 +0100

    fuse_metrics function added to iteratively fuse the MassFunctions

--- ds.py
+++ ds.py
@@ -16,3 +16,27 @@ class DempsterShafer:
 
         # max(ds_dict.keys(), key=(lambda key: ds_dict[key]))
         return ds_dict
+
+    def fuse_metrics(self, m_last, mf_list):
+        count = 0
+        for m_list in mf_list:
+
+            if count == 0:
+                # print('Last m is: {}, the first m in list is: {}'.format(m_last, m_list))
+                result = m_last.combine_disjunctive(m_list)
+                # print(result)
+                result = self.process_ds(result)
+                m = MassFunction(result)
+                count += 1
+                # print(m)
+            # elif count == len(MF_list):
+            #     return m
+            else:
+                # print('Last m is: {}, the first m in list is: {}'.format(m, m_list))
+                result = m.combine_disjunctive(m_list)
+                # print(result)
+                result = self.process_ds(result)
+                m = MassFunction(result)
+                # print(m)
+                count += 1
+        return m
