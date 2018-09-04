a9f377aca54e7e6d3f3b4cdc6e289f80cd1f38a3
jim <j.matthews-14@student.lboro.ac.uk>
2018-09-04 16:24:26 +0100

    function definitions added for whole class

--- select_metrics.py
+++ select_metrics.py
@@ -2,18 +2,34 @@ import itertools
 
 
 class SelectMetrics:
+    """
+    This class determines if the integer entered by the user is valid and what metrics will be
+    processed based on the ineteger.
+    """
 
     def __init__(self, metric_val):
         self._metric_val = metric_val
 
     # Function will validate the inputted metric value
     def validate(self):
+        """
+        This function validates the metric integer by ensuring it is between 1 and 31 and not a float
+
+        :return: True/False: returns a bool based on the result of validating the integer
+        """
+
         if 1 <= self._metric_val <= 31:
             return True
         else:
             return False
 
     def metric_combination(self):
+        """
+        This function determines what metrics are going to be analysed based on the integer that was
+        entered
+
+        :return: metrics: list of strings of each metric to be analysed.
+        """
         numbers = [1, 2, 4, 8, 16]
 
         # this will return the combination of numbers that sum to select_metrics integer
