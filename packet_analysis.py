7bd03f6389c37e0145e90d26967a3849992c171d
jim <j.matthews-14@student.lboro.ac.uk>
2018-09-04 16:08:39 +0100

    function definitions added for whole class

--- packet_analysis.py
+++ packet_analysis.py
@@ -7,7 +7,10 @@ from autobpa import AutoBPA
 
 class PacketAnalysis:
     """
-
+    This class does the main processinf of the packets for offline mode. It uses the filtered packets
+    and uses instances of PacketStatistics and AutoBPA to produce statistics on the metric data
+    which is used to calculate the BPA. The metric BPAs are then fused using Dempster Shafer to produce
+    final result which determines either sliding window or delete frame data.
     """
 
     def __init__(self, array_dict, sw_dict, sw_val, features_to_analyse, **kwargs):
@@ -19,12 +22,16 @@ class PacketAnalysis:
 
     def process_packets(self):
         """
+        This function loops through every packet, calculates the statistics for the metrics, produces BPA
+        values then fuses BPAs for each metric into a final result. It then decides whether to initiate
+        the sliding window or delete the frame data.
 
+        :param:
         :return:
         """
         ds = DempsterShafer()
         inst_bpa = AutoBPA
-        print(inst_bpa)
+        # print(inst_bpa)
 
         # create dynamic instances of AutoBPA class
         instance_dict, ttl_array = inst_bpa.create_instance(self._features_to_analyse,
@@ -60,7 +67,7 @@ class PacketAnalysis:
                 # N, A, U (BPA) are returned in a dictionary
                 # that can be inputted into MF class
                 ds_dict = instance_dict[inst[0]].combined_value(value=array[1][incr])
-
+                # print('ds_dict for metric {}: {}'.format(inst[0], ds_dict))
                 # create instance of MF for each metric with the BPA value dictionary
                 m = MassFunction(ds_dict)
 
@@ -81,8 +88,9 @@ class PacketAnalysis:
 
             # find the maximum out of N, A, U for the combined DS values
             bpa_result = max(result.keys(), key=(lambda key: result[key]))
-
+            # print(bpa_result)
             if 'n' in bpa_result:
+                # print('Reached n')
                 # print(self._sw_dict)
                 # print('self._sw_dict before sliding_window function: {}'.format(self._sw_dict))
                 # print('\n\n')
@@ -90,7 +98,7 @@ class PacketAnalysis:
                 # print('self._sw_dict after sliding_window function: {}'.format(self._sw_dict))
                 # print('\n\n')
                 instance_dict, ttl_array = inst_bpa.create_instance(self._features_to_analyse,
-                                                         self._sw_dict, self._sw_val)
+                                                                    self._sw_dict, self._sw_val)
                 # print('self._sw_dict after new AutoBPA instances created function: {}'.format(self._sw_dict))
                 # print('\n\n')
                 # print(instance_dict)
@@ -109,6 +117,7 @@ class PacketAnalysis:
             elif 'u' in bpa_result:
                 pass
             else:
+                print('Reached else and pass')
                 pass
 
             # if pkt_count == 700:
@@ -124,8 +133,11 @@ class PacketAnalysis:
 
     def sliding_window(self, incr):
         """
+        This function moves the current metric windows along one place when the frame has been
+        identified as Normal. If the current frame is at the end of the pcap file then the window
+        cannot be moved along.
 
-        :param incr:
+        :param incr: current frame increment value. Actually begins at the length of sliding window, not 0
         :return:
         """
         # for x in range(start_value, stop=(len(metric_array)-window_size), step=1):
@@ -145,8 +157,8 @@ class PacketAnalysis:
 
     def delete_frame_data(self, count):
         """
-
-        :param count:
+        This function deletes the current frame if it has been identified to be malicious, i.e. Attack.
+        :param count: frame that needs to be deleted
         :return:
         """
         # print('ATTACK detected in packet {}. Closing web browser!'.format(count))
@@ -160,6 +172,8 @@ class PacketAnalysis:
 
     def debug_file(self):
         """
+        This function will produce the debug file. It will write all necessary data to a file to enable
+        debugging of the performance of the program.
 
         :return:
         """
@@ -172,182 +186,4 @@ class PacketAnalysis:
         # Averages for each metric
         # Final result for frame
         # Current number of malicious frames detected
-
-    # def list_to_array(self, lists):
-    #     lists = [lists]
-    #
-    #     for _list in lists:
-    #
-    # def data_to_csv(self):
-    #     # Convert all packet feature lists to numpy arrays
-    #     dbm_antsignal = np.asarray(dbm_antsignal)
-    #     datarate = np.asarray(datarate)
-    #     duration = np.asarray(duration)
-    #     seq = np.asarray(seq)
-    #     ttl = np.asarray(ttl)
-    #
-    #     # Write all packet feature data to .csv file
-    #     pkt_incr = 0
-    #     pkt_data = np.zeros((len(dbm_antsignal), 5))
-    #
-    #     while pkt_incr < len(dbm_antsignal):
-    #         pkt_data[pkt_incr][0] = dbm_antsignal[pkt_incr]
-    #         pkt_data[pkt_incr][1] = datarate[pkt_incr]
-    #         pkt_data[pkt_incr][2] = duration[pkt_incr]
-    #         pkt_data[pkt_incr][3] = seq[pkt_incr]
-    #         pkt_data[pkt_incr][4] = ttl[pkt_incr]
-    #         # print(dbm_antsignal[pkt_incr], datarate[pkt_incr], duration[pkt_incr], seq[pkt_incr], ttl[pkt_incr])
-    #         pkt_incr += 1
-    #
-    #     np.savetxt('data.csv', pkt_data, fmt='%1.10e', delimiter='\t')
-    #
-    # def sliding_window(self, array, sw_val, incr_val):
-    #     sw_dbm_antsignal = dbm_antsignal[0:sw_size]
-    #     sw_datarate = datarate[0:sw_size]
-    #     sw_duration = duration[0:sw_size]
-    #     sw_seq = seq[0:sw_size]
-    #     sw_ttl = ttl[0:sw_size]
-
-
-
-
-
-# import csv
-# import numpy as np
-# from ds import DempsterShafer
-# from pyds.pyds import MassFunction
-# from autobpa import AutoBPA
-#
-#
-# class PacketAnalysis:
-#
-#     def __init__(self, array_dict, sw_dict, sw_val, features_to_analyse, instance_dict, pkt_count, attack_count):
-#         self._array_dict = array_dict
-#         self._sw_dict = sw_dict
-#         self._sw_val = sw_val
-#         self._features_to_analyse  = features_to_analyse
-#         self._instance_dict = instance_dict
-#         self._pkt_count = pkt_count
-#         self._attack_count = attack_count
-#
-#     def process_packets(self, ds_inst, inst_bpa, incr):
-#         # ds = DempsterShafer()
-#         # inst_bpa = AutoBPA
-#         #
-#         # # create dynamic instances of AutoBPA class
-#         # instance_dict = inst_bpa.create_instance(self._features_to_analyse,
-#         #                                          self._sw_dict, self._sw_val)
-#         # # function variables
-#         # start, stop, step = self._sw_val, len(self._array_dict['RSSI']), 1
-#         # pkt_count = 0
-#         # attack_count = 0
-#         #
-#         # print('The first 0->29 packets are used '
-#         #       'to develop the normal network behaviour.')
-#         # # consider trying to multithread/multiprocess this secion, or use itertools to speed up the process.
-#         # # potentially use itertools.product and dict.keys(), dict.values(), dict.items() to loop through them all
-#         # for incr in range(start, stop, step):
-#         # potentially replace the following 2 for loops and if statement with zip function. As long as array_dict
-#         # and instance_dict have the same arrays in them in the same order it should work fine.
-#         # potentially use itertools.compress to remove redundant arrays, i.e. metrics, that we are
-#         # not going to process
-#
-#         # MF_list = []
-#         # print('Packet number {}'.format(pkt_count))
-#         # ds = DempsterShafer
-#         MF_list = []
-#         for array, inst in zip(self._array_dict.items(), self._instance_dict.items()):
-#             # print('Metric: {}, value: {}'.format(inst[0],array[1][incr]))
-#
-#             # data_val = array[1][incr]
-#             #     inst_auto_bpa = instance_dict[inst[0]]
-#             self._instance_dict[inst[0]].mean()
-#             self._instance_dict[inst[0]].distance(array[1][incr])
-#             self._instance_dict[inst[0]].box_plot()
-#
-#             # N, A, U (BPA) are returned in a dictionary
-#             # that can be inputted into MF class
-#             ds_dict = self._instance_dict[inst[0]].combined_value(value=array[1][incr])
-#
-#             # create instance of MF for each metric with the BPA value dictionary
-#             m = MassFunction(ds_dict)
-#
-#             # append all metrics to list except the last one
-#             if not len(MF_list) == (len(self._features_to_analyse) - 1):
-#                 MF_list.append(m)
-#         #print(MF_list)
-#         print(m)
-#         # Combine all the mass functions into one result for N, A, U
-#         result = ds_inst.fuse_metrics(m_last=m, mf_list=MF_list)
-#
-#         # find the maximum out of N, A, U for the combined DS values
-#         bpa_result = max(result.keys(), key=(lambda key: result[key]))
-#
-#         if 'n' in bpa_result:
-#             self.sliding_window(self._pkt_count)
-#             self._instance_dict = inst_bpa.create_instance(self._features_to_analyse,
-#                                                            self._sw_dict, self._sw_val)
-#         elif 'a' in bpa_result:
-#             self._attack_count += 1
-#             print('ATTACK detected in packet {}. Closing web browser!'.format(self._pkt_count))
-#             self.delete_frame_data(incr)
-#         # elif 'u' in bpa_result:
-#         #     continue
-#         # else:
-#         #     continue
-#
-#         self._pkt_count += 1
-#
-#         print(self._attack_count)
-#
-#     def sliding_window(self, start_value):
-#         # start_value = start_value + 30
-#         # for x in range(start_value, stop=(len(metric_array)-window_size), step=1):
-#         for norm_arrays, sw_arrays in zip(self._array_dict.values(), self._sw_dict.items()):
-#             self._sw_dict[sw_arrays[0]] = norm_arrays[start_value:(start_value + self._sw_val)]
-#
-#         # return self._sw_dict
-#
-#     def delete_frame_data(self, count):
-#         print('ATTACK detected in packet {}. Closing web browser!'.format(count))
-#         for metric, array in self._array_dict.items():
-#             # check the count value includes the sw_size and is incremented in the correct place
-#             self._array_dict[metric] = np.delete(array, count)
-#
-#         # return self._array_dict
-#
-#     # def list_to_array(self, lists):
-#     #     lists = [lists]
-#     #
-#     #     for _list in lists:
-#     #
-#     # def data_to_csv(self):
-#     #     # Convert all packet feature lists to numpy arrays
-#     #     dbm_antsignal = np.asarray(dbm_antsignal)
-#     #     datarate = np.asarray(datarate)
-#     #     duration = np.asarray(duration)
-#     #     seq = np.asarray(seq)
-#     #     ttl = np.asarray(ttl)
-#     #
-#     #     # Write all packet feature data to .csv file
-#     #     pkt_incr = 0
-#     #     pkt_data = np.zeros((len(dbm_antsignal), 5))
-#     #
-#     #     while pkt_incr < len(dbm_antsignal):
-#     #         pkt_data[pkt_incr][0] = dbm_antsignal[pkt_incr]
-#     #         pkt_data[pkt_incr][1] = datarate[pkt_incr]
-#     #         pkt_data[pkt_incr][2] = duration[pkt_incr]
-#     #         pkt_data[pkt_incr][3] = seq[pkt_incr]
-#     #         pkt_data[pkt_incr][4] = ttl[pkt_incr]
-#     #         # print(dbm_antsignal[pkt_incr], datarate[pkt_incr], duration[pkt_incr], seq[pkt_incr], ttl[pkt_incr])
-#     #         pkt_incr += 1
-#     #
-#     #     np.savetxt('data.csv', pkt_data, fmt='%1.10e', delimiter='\t')
-#     #
-#     # def sliding_window(self, array, sw_val, incr_val):
-#     #     sw_dbm_antsignal = dbm_antsignal[0:sw_size]
-#     #     sw_datarate = datarate[0:sw_size]
-#     #     sw_duration = duration[0:sw_size]
-#     #     sw_seq = seq[0:sw_size]
-#     #     sw_ttl = ttl[0:sw_size]
 
