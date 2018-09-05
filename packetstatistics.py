2b577d85b2bf65cddb9c71eedd422bd96cbe9538
Jim-Matthews <j.matthews-14@student.lboro.ac.uk>
2018-09-05 17:10:10 +0100

    cleanup of comments

--- packetstatistics.py
+++ packetstatistics.py
@@ -2,22 +2,13 @@ import statistics
 import numpy as np
 
 
-# PacketStatistics needs the sw_val for the box plot function.
-# Pass in as initialising variable like local binary patterns function for project
 class PacketStatistics:
     """
     This class contains functions that apply some statistical method on data, such as: mean, mode, median,
     euclidean distance and box plot. It is also the parent class of AutoBPA.
     """
-    # may need to set __init__ up as **kwargs to ensure that the median,
-    # and quartile values are not required
-    # when initiating the class
 
-    # def __init__(self, sw, median, lower_quart, upper_quart, inter_quart_range):
     def __init__(self, data, **kwargs):
-        # These are object variables, they aren't initialised until after the object is defined.
-        # Use kwargs so they don't have to be initialised in the correct order.
-        # Set to default to 0 if they aren't set in the class instance?
         self._data = data
         if 'mean' in kwargs: self._mean = kwargs['mean']
         if 'dist_mean' in kwargs: self._dist_mean = kwargs['dist_mean']
@@ -35,8 +26,7 @@ class PacketStatistics:
         :return: self._mean
         """
         self._mean = statistics.mean(self._data)
-        # print('SW data being analysed is: {}'.format(self._data))
-        # print('Mean value is: {}'.format(self._mean))
+
         return self._mean
 
     def frequency(self):
@@ -69,15 +59,6 @@ class PacketStatistics:
 
         self._dist_mean = abs(self._mean - new_val)
 
-        # Uncommenting this section assigns 0.01 to a lot of metric processing
-        # BPA values. This then gives A = 0.5, which is too
-        # high to be justified given the conditions.
-
-        # if self._dist_maxval == 0:
-        #     self._dist_maxval = 0.01
-        # if self._dist_mean == 0:
-        #     self._dist_mean = 0.01
-
     def box_plot(self):
         """
         This function calculates the box plot quartiles of the data set, i.e. lower quartile (LQ),
@@ -85,17 +66,9 @@ class PacketStatistics:
 
         :return:
         """
-        # produces the quartiles and stores them in self.variable_name
-        # autoBPA will inherit from the statistics class and therefore will have access to
-        # the quartile values through the self.__init__ inheritance thing. AutoBPA will use
-        # the current packet and self. variables to determine the probability assignment.
 
         sorted_data = np.sort(self._data)
-        # print('sorted data is: {}'.format(sorted_data))
-        # print('length of sorted data is: {}'.format(len(sorted_data)))
         self._lower_quart = sorted_data[int((self._sw + 1) / 4)]
-        # print('lower quartile is: {}'.format(self._lower_quart))
-
         self._upper_quart = sorted_data[int(3 * ((self._sw + 1) / 4))]
         self._inter_quart_range = int(self._upper_quart - self._lower_quart)
-        # print(self._lower_quart, self._upper_quart, self._inter_quart_range)
+
